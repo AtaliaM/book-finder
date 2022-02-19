00a2c36aa9d24d036390535652255fc60431b40c
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2022-02-19 18:38:39 +0200

    added header, added expand description feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
-import googleApi from './apis/googleApi';
 import logo from './logo.png';
 import SearchPage from './components/SearchPage/SearchPage';
 
 class App extends React.Component {
-
-  async componentDidMount() {
-    const res = await googleApi.get(`pride and prejudice`);
-    console.log(res.data);
-  }
 
   render() {
     return (
