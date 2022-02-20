a80704d9d02893249753a304625560fa155deba0
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2022-02-20 12:40:23 +0200

    added loader

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -5,20 +5,21 @@ import googleApi from "../../apis/googleApi";
 
 class SearchPage extends React.Component {
 
-    state = { noSearchResults: false, noSearchTermMsg: false, results: [] }
+    state = { noSearchResults: false, noSearchTermMsg: false, results: [], showLoader: false }
 
     onSearch = async (e, searchTerm) => {
         e.preventDefault();
         try {
+            this.setState({showLoader:true})
             if(searchTerm.length > 0) {
                 const res = await googleApi.get(searchTerm);
                 if (res.data.totalItems > 0) {
-                    this.setState({ noSearchResults: false, noSearchTermMsg: false, results: [...res.data.items] });
+                    this.setState({ noSearchResults: false, noSearchTermMsg: false, results: [...res.data.items], showLoader:false });
                 } else {
-                    this.setState({ noSearchResults: true, noSearchTermMsg: false});
+                    this.setState({ noSearchResults: true, noSearchTermMsg: false, showLoader:false});
                 }
             } else {
-                this.setState({noSearchTermMsg:true, noSearchResults: false});
+                this.setState({noSearchTermMsg:true, noSearchResults: false, showLoader:false});
             }
         } catch(e) {
             console.log(e);
@@ -29,6 +30,7 @@ class SearchPage extends React.Component {
         return (
             <>
                 <SearchField onSearch={this.onSearch} />
+                {this.state.showLoader ? <div className="ui active centered inline loader"></div> : null}
                 {this.state.noSearchResults ? <div>No results for this term</div> : null}
                 {this.state.noSearchTermMsg ? <div>Please enter something to search</div> : null}
                 {this.state.results.length > 0 ?
